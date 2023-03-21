7ea1b7cfe57a215cbcaa8937939abed1ab2f12b6
hz <hedzrz@gmail.com>
2023-03-21 11:39:30 +0800

    added git.mk

---
+++ git.mk
@@ -0,0 +1,6 @@
+
+GIT_VERSION  := $(shell git describe --tags --abbrev=0 2>/dev/null || echo "v0.0.0")
+GIT_REVISION := $(shell git rev-parse --short HEAD)
+GIT_SUMMARY=$(shell git describe --tags --dirty --always)
+GIT_DESC=$(shell git log --oneline -1)
+GIT_HASH=$(shell git rev-parse HEAD)
